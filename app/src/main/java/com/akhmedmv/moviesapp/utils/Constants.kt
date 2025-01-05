b2fa8d3c0fcfa7f5247412ce14ff1b88dac93229
Akhmedmv <ahmazelem@gmail.com>
2025-01-05 06:10:43 +0300

    feat: setup navigation with splash, main, and details screens

---
+++ app/src/main/java/com/akhmedmv/moviesapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.akhmedmv.moviesapp.utils
+
+class Constants {
+    object Screens {
+        const val SPLASH_SCREEN = "splash_screen"
+        const val MAIN_SCREEN = "splash_screen"
+        const val DETAILS_SCREEN = "splash_screen"
+    }
+}
